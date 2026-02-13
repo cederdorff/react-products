f8b2b20fff4375b92df6fc494947f37ece439f88
RACE - Rasmus Cederdorff <rasmus@cederdorff.com>
2026-02-13 09:29:00 +0100

    fix: update Vite config to use dynamic base path based on command

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// https://vitejs.dev/config/
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [react()],
+    base: command === "serve" ? "/" : "/YOUR-REPO-NAME/" // 👈 Replace with your GitHub repository name
+  };
+});
